3105170aa9b8d52a941d73ff6c6a640990ac4816
Oscar Yu <oscar@sensortransport.com>
2020-03-07 15:51:32 -0800

    Update make-portfolio-public.py

--- make-portfolio-public.py
+++ make-portfolio-public.py
@@ -1,27 +1,28 @@
 import json
-import logging
 import boto3
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
     codepipeline = boto3.client('codepipeline')
+    print("**************** This is the event - start ****************")
+    print(event)
+    print("**************** This is the event - end ****************")
     job_id = event['CodePipeline.job']['id']
-    
+
     try:
         portfolio_bucket = s3.Bucket('portfolio.preskofan.com')
         for obj in portfolio_bucket.objects.all():
             print obj.key
             portfolio_bucket.Object(obj.key).Acl().put(ACL='public-read')
             user_parameters = event['CodePipeline.job']['data']['actionConfiguration']['configuration']['UserParameters']
-            logger.info('User parameters: {user_parameters}')
             response = codepipeline.put_job_success_result(jobId=job_id)
-            logger.debug(response)
+            print(response)
             return {
                 'statusCode': 200,
                 'body': json.dumps('Hello from Lambda!')
             }
     except Exception as error:
-        logger.exception(error)
+        print(error)
         response = codepipeline.put_job_failure_result(
             jobId=job_id,
             failureDetails={
@@ -29,4 +30,4 @@ def lambda_handler(event, context):
                 'message': '{error.__class__.__name__}: {str(error)}'
             }
         )
-        logger.debug(response)
+        print(response)
