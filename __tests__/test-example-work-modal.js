0c8f1ad5f25932336cf6870677732c968e560b6b
Oscar Yu <oscar@sensortransport.com>
2020-03-14 07:20:29 -0700

    more testing

--- __tests__/test-example-work-modal.js
+++ __tests__/test-example-work-modal.js
@@ -8,7 +8,7 @@ Enzyme.configure({adapter:new Adapter()});
 
 const myExample = {
     'title': "Work Example",
-    'href': 'http://example.com',
+    'href': 'https://example.com',
     'desc': 'Fugiat nulla Lorem laborum cupidatat voluptate anim anim nisi veniam culpa ullamco. Et consectetur Lorem nostrud fugiat quis laborum qui do sunt. Ad eiusmod occaecat ipsum officia do consectetur laborum nisi in consectetur esse. Ex do pariatur exercitation enim mollit occaecat laboris proident est. Quis voluptate aute laboris fugiat excepteur id incididunt sunt cupidatat. Occaecat cillum reprehenderit consectetur incididunt velit et aliquip consequat do.',
     'image': {
         'desc': "example screenshot of a project involving code",
@@ -24,5 +24,10 @@ describe("ExampleWorkModal component", () => {
     it("Should be contain a single 'a' element", () => {
         // console.log(component.debug());
         expect(anchors.length).toEqual(1);
+    });
+
+    it("Should link to our project", () => {
+        // expect(anchors.node.props.href).toEqual(myExample.href);
+        expect(anchors.prop('href')).toEqual(myExample.href);
     });
 });
