638735e26d887b1002886bdfba997075446dfc7d
Oscar Yu <oscar@sensortransport.com>
2020-03-14 07:15:01 -0700

    modal content

---
+++ __tests__/test-example-work-modal.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExampleWorkModal from '../js/example-work-modal';
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter:new Adapter()});
+
+const myExample = {
+    'title': "Work Example",
+    'href': 'http://example.com',
+    'desc': 'Fugiat nulla Lorem laborum cupidatat voluptate anim anim nisi veniam culpa ullamco. Et consectetur Lorem nostrud fugiat quis laborum qui do sunt. Ad eiusmod occaecat ipsum officia do consectetur laborum nisi in consectetur esse. Ex do pariatur exercitation enim mollit occaecat laboris proident est. Quis voluptate aute laboris fugiat excepteur id incididunt sunt cupidatat. Occaecat cillum reprehenderit consectetur incididunt velit et aliquip consequat do.',
+    'image': {
+        'desc': "example screenshot of a project involving code",
+        'src': "images/example1.png",
+        'comment': ""
+    }
+};
+
+describe("ExampleWorkModal component", () => {
+    let component = shallow(<ExampleWorkModal example={myExample} />);
+    let anchors = component.find('a');
+
+    it("Should be contain a single 'a' element", () => {
+        // console.log(component.debug());
+        expect(anchors.length).toEqual(1);
+    });
+});
--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
     {
         'title': "Work Example",
+        'href':'http://example.com',
+        'desc':'Fugiat nulla Lorem laborum cupidatat voluptate anim anim nisi veniam culpa ullamco. Et consectetur Lorem nostrud fugiat quis laborum qui do sunt. Ad eiusmod occaecat ipsum officia do consectetur laborum nisi in consectetur esse. Ex do pariatur exercitation enim mollit occaecat laboris proident est. Quis voluptate aute laboris fugiat excepteur id incididunt sunt cupidatat. Occaecat cillum reprehenderit consectetur incididunt velit et aliquip consequat do.',
         'image': {
             'desc': "example screenshot of a project involving code",
             'src': "images/example1.png",
@@ -13,6 +15,8 @@ const myWork = [
     },
     {
         'title': "Portfolio Boilderplate",
+        'href':'http://example.com',
+        'desc':'Proident sunt minim deserunt labore ut ipsum. Anim commodo deserunt officia quis Lorem sunt cupidatat. Ullamco laborum Lorem et ex elit velit excepteur voluptate exercitation consequat ex dolor. Velit reprehenderit aute voluptate labore qui irure eu esse aute consectetur irure. Sit in elit ad magna voluptate.',
         'image': {
             'desc': "A Serverless Portfolio",
             'src': "images/example2.png",
@@ -21,6 +25,8 @@ const myWork = [
     },
     {
         'title': "Work Example",
+        'href':'http://example.com',
+        'desc':'Irure in amet in commodo elit enim velit voluptate. Nisi consectetur cillum minim ipsum exercitation commodo et sint aliqua. Eiusmod deserunt enim fugiat ullamco deserunt sint commodo ea in id laboris ex laboris. Ullamco culpa adipisicing tempor exercitation veniam cillum veniam. Ut excepteur tempor culpa dolor excepteur eu nostrud excepteur esse. Pariatur laboris dolor aliqua nulla enim cillum id fugiat amet sunt non ad nostrud pariatur. Tempor minim pariatur officia in culpa elit elit.',
         'image': {
             'desc': "example screenshot of a project involving cats",
             'src': "images/example3.png",
